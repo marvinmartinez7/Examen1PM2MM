281cecda637026f52424aff295b9609b910dce40
marvi <marvi@DELL>
2022-10-22 16:20:37 -0600

    Agregar archivos de proyecto.

---
+++ Examen1PM2MM/Examen1PM2MM/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Examen1PM2MM.Controllers;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen1PM2MM
+{
+    public partial class App : Application
+    {
+        static UbicacionesDB basedatos;
+        public static UbicacionesDB BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new UbicacionesDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "UbicacionesDB.db3"));
+                }
+
+                return basedatos;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Examen1PM2MM/Examen1PM2MM/Models/Ubicaciones.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen1PM2MM.Models
+{
+    public class Ubicaciones
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public byte[] Imagen { get; set; }
+
+        public float Longitud { get; set; }
+
+        public float Latitud { get; set; }
+
+        [MaxLength(200)]
+        public String Descripcion { get; set; }
+    }
+}
